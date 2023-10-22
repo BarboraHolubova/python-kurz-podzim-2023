23a1c96511814f92f7be35da7b92b2b31a1a40ee
Barbora Holubova <baissa@centrum.cz>
2023-10-22 19:17:22 +0200

    Úkol 3

---
+++ ukol-3.py
@@ -0,0 +1,31 @@
+import json
+
+with open("body.json", mode="r", encoding="utf-8") as file:
+    data = json.load(file)
+
+
+hodnoceni = {}
+
+
+for student, hodnota in data.items():
+    if hodnota >= 60:
+        hodnota = "Pass"
+        hodnoceni[hodnota].append(student)
+        print(hodnoceni)
+
+#    else:
+ #       hodnota = "Fail"
+  #      hodnoceni[hodnota].append[student]
+   #     print(hodnoceni)
+
+
+
+
+with open("prospech.json", mode="w", encoding="utf-8") as output_file:
+    json.dump(hodnoceni, output_file, indent=4, ensure_ascii=False)
+
+"""
+Z písemky nebude známka, ale jen Pass/Fail hodnocení neboli prospěl(a)/neprospěl(a). 
+Vytvoř nový slovník. Jeho klíče budou opět jména žáků, a hodnotou bude řetězec "Pass", 
+pokud má jednotlivec alespoň než 60 bodů. Pokud má méně než 60, hodnota bude "Fail".
+"""
