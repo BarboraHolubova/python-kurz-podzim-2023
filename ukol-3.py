e6d0d884ebd127221ae9781c004d2c4f99e4026e
Barbora Holubova <baissa@centrum.cz>
2023-10-23 12:18:35 +0200

    Úkol 3

--- ukol-3.py
+++ ukol-3.py
@@ -3,23 +3,16 @@ import json
 with open("body.json", mode="r", encoding="utf-8") as file:
     data = json.load(file)
 
-
 hodnoceni = {}
-
 
 for student, hodnota in data.items():
     if hodnota >= 60:
         hodnota = "Pass"
-        hodnoceni[hodnota].append(student)
-        print(hodnoceni)
-
-#    else:
- #       hodnota = "Fail"
-  #      hodnoceni[hodnota].append[student]
-   #     print(hodnoceni)
-
-
-
+    else:
+        hodnota = "Fail"
+    if hodnota not in  hodnoceni:
+        hodnoceni[student] = hodnota
+print(hodnoceni)
 
 with open("prospech.json", mode="w", encoding="utf-8") as output_file:
     json.dump(hodnoceni, output_file, indent=4, ensure_ascii=False)
